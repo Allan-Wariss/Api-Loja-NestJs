72920a04cf8da3080e22b803ff5031e3489bf309
Allan Wariss <102264563+Allan-Wariss@users.noreply.github.com>
2025-11-03 08:45:28 -0300

    feat: config docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
+import { ConfigurableModuleBuilder, Module } from '@nestjs/common';
 import { usuarioModule } from './usuario/usuario.module';
 import { produtoModule } from './produto/produto.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [usuarioModule, produtoModule],
+  imports: [
+    usuarioModule,
+    produtoModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
 })
 export class AppModule {}
